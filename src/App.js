e18ce14414e27ce9592fdfc4e3165daf3d8fd846
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-03-13 12:44:18 +0100

    add illustration and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
+import Illustration from "./Images/Illustration.gif";
 import './App.css';
+
+
 
 function App() {
   return (
+
     <div className="App">
-    <h1>Budget App</h1>
-    </div>
+    <h1>Budget Meditation</h1>
+    <img src={ Illustration }></img>
+    <p>Illustration: Dazzle Line by Anna Żołnierowicz</p>
+   </div>
+  
   );
 }
 
