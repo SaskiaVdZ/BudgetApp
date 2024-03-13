15447c3db6ad8192abbfc496875d0a95bef2b2b7
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-03-13 12:56:52 +0100

    install mui and change to mui components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import Illustration from "./Images/Illustration.gif";
 import './App.css';
+import { Typography, Container } from '@mui/material';
 
 
 
 function App() {
   return (
-
-    <div className="App">
-    <h1>Budget Meditation</h1>
+    <Container>
+    <Typography variant="h3" >Budget Meditation</Typography>
     <img src={ Illustration }></img>
-    <p>Illustration: Dazzle Line by Anna Żołnierowicz</p>
-   </div>
-  
+   <Typography variant="subtitle1">Illustration: Dazzle Line by Anna Żołnierowicz</Typography>
+   </Container>
   );
 }
 
